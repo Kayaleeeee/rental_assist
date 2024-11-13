fa2e03479703a22a6f5657f7957194760fbe864a
kayalee <gayeon71057@gmail.com>
2024-11-13 18:25:59 +0900

    Button style 추가

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -3,18 +3,25 @@ import styles from "./index.module.scss";
 
 type Props = PropsWithChildren<{
   size: "Small" | "Medium" | "Large";
+  variant?: "outlined" | "filled" | "text";
 }> &
   HTMLAttributes<HTMLButtonElement>;
 
 export const Button = (props: Props) => {
-  const getStyle = () => {
+  const getSize = () => {
     if (props.size === "Small") return styles.smallButton;
     if (props.size === "Medium") return styles.mediumButton;
     return styles.largeButton;
   };
 
+  const getStyle = () => {
+    if (props.variant === "outlined") return styles.outlinedButton;
+    if (props.variant === "text") return styles.textButton;
+    return styles.filledButton;
+  };
+
   return (
-    <button className={getStyle()} {...props}>
+    <button className={`${getSize()} ${getStyle()}`} {...props}>
       {props.children}
     </button>
   );
