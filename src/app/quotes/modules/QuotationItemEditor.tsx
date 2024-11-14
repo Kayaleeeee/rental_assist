2e0eda82dde3643303cf1f17b94e5339eb129940
kayalee <gayeon71057@gmail.com>
2024-11-14 23:56:21 +0900

    quoteItem qty 추가

--- src/app/quotes/modules/QuotationItemEditor.tsx
+++ src/app/quotes/modules/QuotationItemEditor.tsx
@@ -1,33 +1,68 @@
-import { EquipmentListItemType } from "@/app/types/equipmentType";
 import styles from "./quotationItemEditor.module.scss";
 import { EditableField } from "@/app/components/EditableField";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import { QuotItemStateType } from "../hooks/useQuoteForm";
+
 type Props = {
   rentalDays: number;
-  equipmentState: EquipmentListItemType;
-  onChangeField: (item: EquipmentListItemType) => void;
+  quoteState: QuotItemStateType;
+  onChangeField: (item: QuotItemStateType) => void;
   onDeleteEquipment: () => void;
 };
 
 export const QuotationItemEditor = ({
-  equipmentState,
+  quoteState,
   rentalDays,
   onChangeField,
   onDeleteEquipment,
 }: Props) => {
+  const onClickPlus = () => {
+    const newQuantity = quoteState.quantity + 1;
+
+    onChangeField({
+      ...quoteState,
+      quantity: newQuantity,
+      totalPrice: quoteState.price * newQuantity,
+    });
+  };
+
+  const onClickMinus = () => {
+    if (quoteState.quantity === 1) return;
+
+    const newQuantity = quoteState.quantity - 1;
+
+    onChangeField({
+      ...quoteState,
+      quantity: newQuantity,
+      totalPrice: quoteState.price * newQuantity,
+    });
+  };
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.title}>{equipmentState.title}</div>
+      <div className={styles.title}>{quoteState.title}</div>
+      <div className={styles.quantity}>
+        <div className={styles.quantityButton} onClick={onClickMinus}>
+          {" "}
+          -{" "}
+        </div>
+        <div className={styles.quantityNumber}>{quoteState.quantity}</div>
+        <div className={styles.quantityButton} onClick={onClickPlus}>
+          {" "}
+          +{" "}
+        </div>
+      </div>
+
       <div className={styles.days}>{rentalDays}일</div>
       <div className={styles.price}>
         <EditableField
           isEditable
-          value={rentalDays * equipmentState.price}
+          value={quoteState.totalPrice}
           onChange={(e) => {
             const changedPrice = Number(e.target.value);
             if (isNaN(changedPrice)) return;
 
-            onChangeField({ ...equipmentState, price: changedPrice });
+            onChangeField({ ...quoteState, totalPrice: changedPrice });
           }}
         />
         원
