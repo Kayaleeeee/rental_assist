6696daaa1dafc213d13d8f75bd64d29a920c0cc4
kayalee <gayeon71057@gmail.com>
2024-11-15 22:32:06 +0900

    네비게이션 로그인, 로그아웃 버튼 변경

--- src/app/components/Button/NavLoginButton.tsx
+++ src/app/components/Button/NavLoginButton.tsx
@@ -1,18 +1,23 @@
 "use client";
 
-import { useClientAuthentication } from "@/app/hooks/auth/useClientAuthentication";
 import Link from "next/link";
 import { Button } from ".";
+import { useAuthStore } from "@/app/store/useAuthStore";
+import { useEffect } from "react";
 
 export const NavLoginButton = () => {
-  const { user, loading } = useClientAuthentication();
+  const { user, loading, fetchUser } = useAuthStore();
+
+  useEffect(() => {
+    fetchUser();
+  }, [fetchUser]);
 
   if (!user && loading) return null;
 
   return (
     <Link href={"/login"}>
       <Button variant="outlined" size="Medium">
-        {!loading && !user ? "로그인" : `${user?.email}님`}
+        {!loading && !user ? "로그인" : "로그아웃"}
       </Button>
     </Link>
   );
