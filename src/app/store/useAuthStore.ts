59a85fb4780c976c17592e386bb7b97d6f87c3a8
kayalee <gayeon71057@gmail.com>
2024-11-15 22:27:06 +0900

    authStore zustand로 변경

---
+++ src/app/store/useAuthStore.ts
@@ -0,0 +1,57 @@
+import { AdminUserType } from "@/app/types/userType";
+import { createClient } from "@/app/utils/supabase/client";
+import { create } from "zustand";
+
+interface AuthState {
+  user: AdminUserType | undefined;
+  error: any;
+  loading: boolean;
+  fetchUser: () => Promise<void>;
+  getUser: () => Promise<AdminUserType | undefined>;
+}
+
+export const useAuthStore = create<AuthState>((set, get) => ({
+  user: undefined,
+  error: null,
+  loading: true,
+  getUser: async () => {
+    const currentUser = get().user;
+    if (currentUser) return currentUser;
+
+    await get().fetchUser();
+    return get().user;
+  },
+  fetchUser: async () => {
+    const supabase = createClient();
+
+    set({ loading: true });
+
+    try {
+      const { data: sessionData, error: sessionError } =
+        await supabase.auth.getSession();
+
+      if (sessionError || !sessionData.session) {
+        set({
+          error: sessionError || new Error("No session found"),
+          user: undefined,
+          loading: false,
+        });
+        return;
+      }
+
+      const { data: userData, error: userError } =
+        await supabase.auth.getUser();
+
+      if (userError) {
+        set({ error: userError, user: undefined, loading: false });
+      } else {
+        set({ user: userData.user || undefined, error: null, loading: false });
+      }
+    } catch (err) {
+      console.error("Error fetching user:", err);
+      set({ error: err, user: undefined, loading: false });
+    } finally {
+      set({ loading: false });
+    }
+  },
+}));
