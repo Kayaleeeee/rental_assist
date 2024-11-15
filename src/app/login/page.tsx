b47fb1a62a2b330d8945f3e5ca8161def66ec19b
kayalee <gayeon71057@gmail.com>
2024-11-15 15:55:57 +0900

    로그인, 회원가입 페이지 추가

---
+++ src/app/login/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { FormWrapper } from "@components/Form/FormWrapper";
+import formStyles from "@components/Form/index.module.scss";
+import { Label } from "@components/Form/Label";
+import { EditableField } from "@components/EditableField";
+import { useState } from "react";
+import { Button } from "@components/Button";
+import { login } from "@actions/auth/login";
+import { Margin } from "@components/Margin";
+import Link from "next/link";
+import styles from "./page.module.scss";
+import { showToast } from "../utils/toastUtils";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleLogin = async () => {
+    try {
+      await login({ email, password });
+      showToast({ message: "로그인 성공", type: "success" });
+    } catch (e) {
+      showToast({ message: "로그인에 실패했습니다", type: "error" });
+    }
+  };
+
+  return (
+    <FormWrapper>
+      <div
+        style={{
+          maxWidth: "500px",
+          marginBottom: "40px",
+          marginTop: "20px",
+        }}
+      >
+        <h1>로그인</h1>
+        <section className={formStyles.sectionWrapper}>
+          <Label title="Email" />
+          <EditableField
+            placeholder="이메일"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </section>
+        <section className={formStyles.sectionWrapper}>
+          <Label title="비밀번호" />
+          <EditableField
+            placeholder="비밀번호를 입력해주세요."
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </section>
+        <Button
+          size="Medium"
+          style={{
+            width: "100%",
+          }}
+          onClick={handleLogin}
+        >
+          로그인
+        </Button>
+        <Margin top={40} bottom={20}>
+          <Link className={styles.signupButton} href={"/signup"}>
+            회원가입 하기
+          </Link>
+        </Margin>
+      </div>
+    </FormWrapper>
+  );
+};
+
+export default LoginPage;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import { FormWrapper } from "@components/Form/FormWrapper";
+import formStyles from "@components/Form/index.module.scss";
+import { Label } from "@components/Form/Label";
+import { EditableField } from "@components/EditableField";
+import { useMemo, useState } from "react";
+import { Button } from "@components/Button";
+import { signup } from "@actions/auth/login";
+import { Margin } from "@components/Margin";
+import Link from "next/link";
+import styles from "./page.module.scss";
+import { CustomError } from "../class/CustomError";
+import { ErrorMessage } from "../components/Message/ErrorMessage";
+import { showToast } from "../utils/toastUtils";
+
+const SignUpPage = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const isPasswordMatch = useMemo(() => {
+    return password === confirmPassword;
+  }, [confirmPassword, password]);
+
+  const handleSignUp = async () => {
+    if (isPasswordMatch) {
+      setErrorMessage("비밀번호가 일치하지 않습니다.");
+      return;
+    }
+
+    try {
+      await signup({ email, password });
+      showToast({
+        message: "회원가입이 완료되었습니다.",
+        type: "success",
+      });
+    } catch (e) {
+      const errorMessage = (e as CustomError).message || "알 수 없는 에러 발생";
+      setErrorMessage(errorMessage);
+    }
+  };
+
+  return (
+    <FormWrapper>
+      <div
+        style={{
+          maxWidth: "500px",
+          marginBottom: "40px",
+          marginTop: "20px",
+        }}
+      >
+        <h1>회원가입</h1>
+        <section className={formStyles.sectionWrapper}>
+          <Label title="Email" />
+          <EditableField
+            placeholder="이메일"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </section>
+        <section className={formStyles.sectionWrapper}>
+          <Label title="비밀번호" />
+          <EditableField
+            placeholder="비밀번호를 입력해주세요."
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </section>
+        <section className={formStyles.sectionWrapper}>
+          <Label title="비밀번호 확인" />
+          <EditableField
+            placeholder="비밀번호를 입력해주세요."
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </section>
+        {errorMessage && (
+          <Margin bottom={20}>
+            <ErrorMessage message={errorMessage} />
+          </Margin>
+        )}
+
+        <Button
+          size="Medium"
+          style={{
+            width: "100%",
+          }}
+          onClick={handleSignUp}
+        >
+          회원가입
+        </Button>
+        <Margin top={40} bottom={20}>
+          <Link className={styles.signupButton} href={"/login"}>
+            로그인 하기
+          </Link>
+        </Margin>
+      </div>
+    </FormWrapper>
+  );
+};
+
+export default SignUpPage;
