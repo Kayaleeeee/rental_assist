b53f521e0e2dc2c28e81d460fe49d9b867283e6d
kayalee <gayeon71057@gmail.com>
2024-11-24 01:15:26 +0900

    variable 변경

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,4 @@
-export const NEXT_PUBLIC_SUPABASE_URL = process.env.SUPABASE_URL || "";
+export const NEXT_PUBLIC_SUPABASE_URL =
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 export const NEXT_PUBLIC_SUPABASE_ANON_KEY =
-  process.env.SUPABASE_ANON_KEY || "";
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
